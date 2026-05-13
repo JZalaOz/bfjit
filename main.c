e208ca9058e4a66620f47513d353793f2a742ff1
JZalaOz <jz@jzalaoz.com>
2026-05-13 12:57:39 +0400

    Refactoring (not done)

--- main.c
+++ main.c
@@ -12,7 +12,7 @@
 #include <unistd.h>
 
 #define READ_BUF_SIZE 256
-#define DA_INIT_CAP 8
+#define DA_INIT_CAP 128
 
 #define DA_APPEND(arr, item)\
     do {\
@@ -40,11 +40,13 @@ typedef enum {
 } Op_kind;
 
 typedef struct {
+    uin32_t fileRow;
+    uint32_t fileColumn;
 	Op_kind kind;
-    // for JUMP op kind, value is the other pairing jump op index in Ops array.
-    // Otherwise its just the count.
+ 
+    // If op is a jump kind, value is the index of the other pairing jump in the Ops array.
+    // Else it is just the count.
 	uint32_t value;
-    size_t readerIndex;
 } Op;
 
 typedef struct {
@@ -58,6 +60,20 @@ typedef struct {
     uint32_t capacity;
 } Tape;
 
+Tape new_tape() {
+    Tape tape = {0};
+    tape.items = malloc(sizeof(uint8_t) * DA_INIT_CAP);
+    tape.capacity = DA_INIT_CAP;
+    return tape;
+}
+
+void tape_try_grow(Tape *tape, uint32_t dataPointer) {
+    while (dataPointer >= tape->capacity) {
+        tape->capacity *= 2;
+        tape->items = realloc(tape->items, sizeof(uint8_t) * tape->capacity);
+    }
+}
+
 typedef struct {
     uint8_t *items;
     uint32_t size;
@@ -68,12 +84,12 @@ void emit_byte(Code *code, uint8_t data) {
     DA_APPEND(code, data);
 }
 
-void emit_bytes(Code *code, int args, ...) {
+void emit_bytes(Code *code, int count, ...) {
     va_list ap;
-    va_start(ap, args);
-    while (args > 0) {
+    va_start(ap, count);
+    while (count > 0) {
         DA_APPEND(code, va_arg(ap, int));
-        args--;
+        count--;
     }
     va_end(ap);
 }
@@ -92,18 +108,18 @@ void emit_num(Code *code, uint8_t byteCount, uint64_t x) {
 }
 
 typedef struct {
+    Op *op;
     uint32_t codeIndex;
-    uint32_t opIndex;
-} CodeMarker;
+} Marker;
 
 typedef struct {
-    CodeMarker *items;
+    Marker *items;
     uint32_t size;
     uint32_t capacity;
-} CodeMarkerStack;
+} MarkerStack;
 
-void push_marker(CodeMarkerStack *stack, Code *code, uint32_t opIndex) {
-    DA_APPEND(stack, {.codeIndex = code->size-1, opIndex});
+void push_marker(MarkerStack *stack, Code *code, Op *op) {
+    DA_APPEND(stack, {op, .codeIndex = code->size-1});
 }
 
 CodeMarker pop_marker(CodeMarkerStack *stack) {
@@ -113,46 +129,20 @@ CodeMarker pop_marker(CodeMarkerStack *stack) {
     return stack->items[--stack->size];
 } 
 
-void tryGrowTape(Tape *tape, uint64_t dataPointer) {
-    while (dataPointer >= tape->capacity) {
-        tape->capacity *= 2;
-        tape->items = realloc(tape->items, sizeof(uint8_t) * tape->capacity);
-    }
-}
-
-    /*emit_bytes(&code, 2, 0x41, 0xbe); // mov r14
-    emit_num_uint32(&code, 69420); // 4 byte little endian data
-
-    // mov eax r14d
-    emit_bytes(&code, 3, 0x44, 0x89, 0xf0);
-
-    // mov rax, tryGrowTape ptr
-    emit_bytes(&code, 2, 0x48, 0xb8);
-    emit_num_uint64(&code, (uint64_t) &tryGrowTape);
-
-    // call
-    emit_bytes(&code, 2, 0xff, 0xd0);
-
-    // ret
-    emit_byte(&code, 0xc3);//*/
-
 void jit(Ops *ops) {
-    Tape tape = {0};
-    tape.items = malloc(sizeof(uint8_t) * DA_INIT_CAP);
-    tape.capacity = DA_INIT_CAP;
-
+    Tape tape = new_tape();
     Code code = {0};
     CodeMarkerStack markerStack = {0};
 
     // r15 = Tape Pointer
     // r14 = Data Index
 
-    // Set r14 to 0
-    emit_bytes(&code, 3, 0x4d, 0x31, 0xf6); // xor r14, r14
-
-    // Move &tape into r15
+    // set r15 to &tape
     emit_bytes(&code, 2, 0x49, 0xbf); // movabs r15
     emit_num(&code, 8, (uint64_t) &tape);
+
+    // Set r14 to 0
+    emit_bytes(&code, 3, 0x4d, 0x31, 0xf6); // xor r14, r14
 
     for (int i = 0; i < ops->size; i++) {
         Op op = ops->items[i];
@@ -165,7 +155,7 @@ void jit(Ops *ops) {
                 emit_bytes(&code, 3, 0x4c, 0x89, 0xf7); // mov rdi, r14
 
                 emit_bytes(&code, 2, 0x48, 0xb8); // movabs rax
-                emit_num_uint64(&code, (uint64_t) &tryGrowTape); // tryGrowTape func ptr
+                emit_num_uint64(&code, (uint64_t) &tape_try_grow); // tryGrowTape func ptr
                 emit_bytes(&code, 2, 0xff, 0xd0); // call tryGrowTape
             } break;
             case LEFT: {
@@ -175,32 +165,26 @@ void jit(Ops *ops) {
                 // dont error if dp <= op.value
                 // jmp if
 
+                // Compare op.value (immediate) and r14 (dataPointer.)
+                // If the this op will result in going under 0, throw error ()
+                // else we operate 
                 if (op.value > DP) {
                     assert(0 && "Cannot move data pointer below 0");
                 }
                 DP -= op.value;
             } break;
-            case INC: {
+            case INC: 
+            case DEC: {
                 uint32_t i = op.value;
                 while (i > 0) {
-                    uint32_t x;
-                    if (i < 256) {
-                        x = i;
+                    uint8_t x = i < 256 ? (uint8_t) i : 255;
+                    i -= x;
+                    if (op.kind == INC) {
+                        emit_bytes(&code, 5, 0x43, 0x80, 0x04, 0x37, x); // add byte [r15+r14], immediate x
                     } else {
-                        x = 255;
-                        i -= x;
+                        emit_bytes(&code, 5, 0x43, 0x80, 0x2c, 0x37, x); // sub byte [r15+r14], immediate x
                     }
-                    emit_bytes(&code, 5, 0x43, 0x80, 0x04, 0x37, i); // add byte [r15+r14], 1
                 }
-                if (op.value == 1) {
-                    emit_bytes(&code, 5, 0x43, 0x80, 0x04, 0x37, 0x01); // add byte [r15+r14], 1
-                } else {
-                    whil
-                    emit_bytes(&code, 
-                }
-            } break;
-            case DEC: {
-                emit_bytes(&code, 5, 0x43, 0x80, 0x2c, 0x37, 0x01); // sub byte [r15+r14], 1
             } break;
             case OUT: {
                 // if op.value == 1 then a = [r15+r14] else a = [r15+r14]*op.value length
@@ -244,11 +228,9 @@ void jit(Ops *ops) {
 }
 
 void interpret(Ops *ops) {
+    Tape tape = new_tape();
     uint32_t DP = 0;
     uint32_t IP = 0;
-    Tape tape = {0};
-    tape.items = malloc(sizeof(uint8_t) * DA_INIT_CAP);
-    tape.capacity = DA_INIT_CAP;
     printf("Interpretting...\n");
     while (IP < ops->size) {
         Op op = ops->items[IP];
@@ -258,10 +240,7 @@ void interpret(Ops *ops) {
             switch(op.kind) {
                 case RIGHT: {
                     DP += op.value;
-                    while (DP >= tape.capacity) {
-                        tape.capacity *= 2;
-                        tape.items = realloc(tape.items, sizeof(uint8_t) * tape.capacity);
-                    }
+                    tape_try_grow(&tape, DP);
                 } break;
                 case LEFT: {
                     if (op.value > DP) {
@@ -287,7 +266,7 @@ void interpret(Ops *ops) {
                 } break;
                 default: {
                     if (op.kind != JUMP_IF_ZERO && op.kind != JUMP_IF_NOT_ZERO) {
-                        fprintf(stderr, "Unknown OP kind %d, %d, %ld", op.kind, op.value, op.readerIndex);
+                        fprintf(stderr, "Unknown OP kind %d, %d, %ld", op.kind, op.value, op.fileIndex);
                         exit(1);
                     }
                 }
@@ -302,71 +281,86 @@ typedef struct {
     uint32_t* items;
     uint32_t size;
     uint32_t capacity;
-} LexerLoopStack;
+} LexerJumpStack;
 
 int main(int argc, char **argv) {
 	assert(argc > 0);
-	if (argc != 2) {
-		printf("Usage: %s <input.bf>\n", argv[0]);
+	if (argc != 3) {
+		printf("Usage: %s <input.bf> <jit/interpret>\n", argv[0]);
 		return 1; 
 	}
 
 	char *path = argv[1];
+    char *executionType = argv[2];
+
 	int file = open(path, 0);
 	size_t file_size = lseek(file, 0, SEEK_END);
 	lseek(file, 0, SEEK_SET);
 
     Ops ops = {0};
-    LexerLoopStack loopStack = {0};
+    LexerJumpStack jumpStack = {0};
 
 	char buf[READ_BUF_SIZE];
 	size_t buf_size;
-    size_t readerIndex = 0;
+    uint32_t fileRow = 0;
+    uint32_t fileColumn = 0;
 	while ((buf_size = read(file, buf, READ_BUF_SIZE)) > 0) {
-		for (int i = 0; i < buf_size; i++) {
+        for (int i = 0; i < buf_size; i++) {
             switch (buf[i]) {
-                case '>': 
-                case '<':
-                case '+':
-                case '-':
-                case '.':
-                case ',': {
-                    Op op = {.kind = buf[i], .value = 1, readerIndex};
+                case RIGHT:
+                case LEFT:
+                case INC:
+                case DEC:
+                case OUT:
+                case IN: {
+                    Op op = {.kind = buf[i], .value = 1, fileRow, fileColumn};
                     while (i+1 < buf_size && op.kind == buf[i+1]) {
                         op.value++;
                         i++;
-                        readerIndex++;
+                        fileColumn++;
                     } 
                     DA_APPEND(&ops, op);
                 } break;
                 case '[': {
-                    Op op = {.kind = '[', readerIndex};
-                    DA_APPEND(&loopStack, ops.size);
+                    Op op = {.kind = '[', fileRow, fileColumn};
+                    DA_APPEND(&jumpStack, ops.size);
                     DA_APPEND(&ops, op);
                 } break;
                 case ']': {
-                    if (loopStack.size == 0) {
+                    if (jumpStack.size == 0) {
                         fprintf(stderr, "Error while parsing file. Found closing loop with no opening loop pair\n");
-                        fprintf(stderr, "readerIndex of unpaired closing loop: %ld\n", readerIndex);
+                        fprintf(stderr, "fileRow: %d, fileColumn: %d\n", fileRow, fileColumn);
                         return 1;
                     }
-                    uint32_t leftIndex = loopStack.items[--jumpStack.size];
-                    Op right = {.kind = ']', .value = leftIndex+1, readerIndex };
+                    uint32_t leftIndex = jumpStack.items[--jumpStack.size];
+                    Op right = {.kind = ']', .value = leftIndex+1, fileRow, fileColumn };
                     DA_APPEND(&ops, right);
                     ops.items[leftIndex].value = ops.size;
                 } break;
             }
-            readerIndex++;
+            if (buf[i] == "\n) {
+                fileRow++;
+                fileColumn = 0;
+            } else {
+                fileColumn++;
+            }
         }
     }
-    free(loopStack.items);
+    free(jumpStack.items);
     if (buf_size < 0) {
         perror("Failed to read file");
         return 1;
     }
 
-    jit(&ops);
-    //interpret(&ops);
+    if (executionType == "jit") {
+        jit(&ops);
+    } else if (executionType == "interpret") {
+        interpret(&ops);
+    } else {
+        fprintf(stderr, "Unknown execution type \"%s\"", executionType);
+        return 1;
+    }
+
     free(ops.items);
     return 0;
 }
